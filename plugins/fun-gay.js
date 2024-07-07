c588516b779cebd503f73e05d47eb2bd29540abb
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-07 11:40:17 -0500

    Update fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,14 +1,11 @@
-let handler = async (m, { conn }) => {
-let vn = './media/gay2.mp3';
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {  
-avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),   
-}), 'error.png', `*🏳️‍🌈 𝙂𝘼𝙔 𝙂𝘼𝙔!! 🏳️‍🌈*`, m)   
-await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, { 
-type: 'audioMessage', 
-ptt: true });
+const handler = async (m, {conn}) => {
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', '*🏳️‍🌈 𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙶𝙰𝚈 🏳️‍🌈*', m);
+  
 };
-handler.help = ['gay']
-handler.tags = ['maker']
-handler.command = /^(gay)$/i
-export default handler
+handler.help = ['gay'];
+handler.tags = ['maker'];
+handler.command = /^(gay)$/i;
+export default handler;
