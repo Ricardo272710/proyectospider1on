997dc6b5142fcbe4253128df0947c58a810fe372
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-10 21:56:34 -0500

    Update gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -1,42 +1,49 @@
-let generateWAMessageFromContent = (await import(global.baileys)).default
-import * as fs from 'fs'
+import {generateWAMessageFromContent} from '@whiskeysockets/baileys'
+import * as fs from 'fs';
+const handler = async (m, {conn, text, participants, isOwner, isAdmin, args}) => {
+const q = m.quoted ? m.quoted : m || m.text || m.sender
+if (q.mtype == 'extendedTextMessage') {
+if (args.length >= 1) {
+text = args.slice(0).join(" ")
+} else if (m.quoted && m.quoted.text) {
+text = m.quoted.text
+} else {
+text = "📣 Anuncio"
+} 
+const users = participants.map((u) => conn.decodeJid(u.id))
+await conn.sendMessage(m.chat, { text: text, mentions: users }, { quoted: m }) 
+//const q = m.quoted ? m.quoted : m || m.text || m.sender
+//const c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
+//const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users})
+//await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+} else {
 
-var handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
-if (!m.quoted && !text) return m.reply(`${lenguajeGB['smsAvisoMG']()} 𝙔 𝙀𝙇 𝙏𝙀𝙓𝙏𝙊?`) 
-try { 
-let users = participants.map(u => conn.decodeJid(u.id))
-let q = m.quoted ? m.quoted : m || m.text || m.sender
-let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
-let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: fkontak, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-} catch {  
-/**
-[ By @NeKosmic || https://github.com/NeKosmic/ ]
-**/  
-let users = participants.map(u => conn.decodeJid(u.id))
-let quoted = m.quoted ? m.quoted : m
-let mime = (quoted.msg || quoted).mimetype || ''
-let isMedia = /image|video|sticker|audio/.test(mime)
-let more = String.fromCharCode(8206)
-let masss = more.repeat(850)
-let htextos = `${text ? text : "*Hola!!*"}`
+// [ By @NeKosmic || https://github.com/NeKosmic/ ]
+
+const users = participants.map((u) => conn.decodeJid(u.id))
+const quoted = m.quoted ? m.quoted : m
+const mime = (quoted.msg || quoted).mimetype || ''
+const isMedia = /image|video|sticker|audio/.test(mime)
+const more = String.fromCharCode(8206)
+const masss = more.repeat(850)
+const htextos = m.quoted && m.quoted.text ? m.quoted.text : m.text ? m.text : text ? text : '📣📣📣'
 if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
 var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: m })
+conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users}, {quoted: m})
 } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
 var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m })
+conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos}, {quoted: m})
 } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: m })
+var mediax = await quoted.download?.();
+conn.sendMessage(m.chat, {audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3`}, {quoted: m})
 } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: m })
+var mediax = await quoted.download?.();
+conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m})
 } else {
-await conn.sendMessage(m.chat, { text : text ? text : '' , mentions: users}, { quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
-//await conn.relayMessage(m.chat, {extendedTextMessage:{text: `${masss}\n${htextos}\n`, ...{ contextInfo: { mentionedJid: users, externalAdReply: { thumbnail: imagen1, sourceUrl: md }}}}}, {})
-}}}
-handler.command = /^(hidetag|n|noti|notificar|notify)$/i
+await conn.relayMessage(m.chat, { extendedTextMessage: {text: `${htextos}`, ...{contextInfo: {mentionedJid: users, externalAdReply: { thumbnail: imagen10, sourceUrl: md}}}}}, {});
+}}
+}
+handler.command = /^(hidetag|notificar|notify)$/i
 handler.group = true
 handler.admin = true
 export default handler
