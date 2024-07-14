0da3dee9121f79e09a79772bdf70354d6f18db8c
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-14 14:36:14 -0500

    Update a-MascInterna4.js

--- plugins/a-MascInterna4.js
+++ plugins/a-MascInterna4.js
@@ -1,18 +1,18 @@
 const handler = async (m, {conn}) => {
-  m.reply(global.MascInterna4);
+  m.reply(global.mascinterna4);
 };
-handler.command = /^(MascInterna4|MascInterna4)$/i;
+handler.command = /^(mascinterna4|mascinterna4)$/i;
 export default handler;
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
-global.MascInterna4 = `
-╭┈┈ ๑❀๑ •  *MASC* • ๑❀๑ ••: 
+global.mascinterna4 = ` 
+╭┈┈ ๑❀๑ •  MASC • ๑❀๑ ••: 
 │
-│      ෆ _Encargado:_   
-│      ෆ _Horario:_ (  )🇲🇽 (  )🇨🇴
-│      ෆ _Mod:_  
+│      ෆ Encargado:   
+│      ෆ Horario: (  )🇲🇽 (  )🇨🇴
+│      ෆ Mod:  
 │ 
 │ㅤㅤ_ʚ Escuadra 1:_ 
 │ㅤ🥷🏻• 
@@ -26,7 +26,7 @@ global.MascInterna4 = `
 │  🥷🏻• 
 │ㅤ🥷🏻• 
 │ 
-│   _⊹ ִֶָ Donador/Donadora De Sala:_  
+│   ⊹ ִֶָ Donador/Donadora De Sala:  
 │ 
 ╰───────────────๑❀๑  `;
 `;
