61defd815010454a1ed8cb9c0368044e816e37e0
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-20 15:15:17 -0500

    Update comandos+18-xnxxdl.js

--- plugins/comandos+18-xnxxdl.js
+++ plugins/comandos+18-xnxxdl.js
@@ -1,66 +1,77 @@
 import fetch from 'node-fetch';
+import axios from 'axios';
 import cheerio from 'cheerio';
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsAvisoAG']()}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 +18 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 #𝙤𝙣 𝙢𝙤𝙙𝙤𝙝𝙤𝙧𝙣𝙮 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n+18 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 #𝙤𝙣 𝙢𝙤𝙙𝙤𝙝𝙤𝙧𝙣𝙮 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀*`
-  if (!args[0]) throw `𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙑𝘼𝙇𝙄𝘿𝙊 𝘿𝙀 𝙓𝙉𝙓𝙓, 𝙀𝙅𝙀𝙈𝙋𝙇𝙊: ${usedPrefix + command} https://www.xnxx.com/video-14lcwbe8/rubia_novia_follada_en_cuarto_de_bano*`
+
+const handler = async (m, { conn, args, command, usedPrefix, text }) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.adult_xvideosdl
+
+  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1} #enable modohorny*`;
+  if (!args[0]) throw `${tradutor.texto2} ${usedPrefix + command} https://www.xvideos.com/video70389849/pequena_zorra_follada_duro*`;
   try {
-    await conn.reply(m.chat, '➤ 𝙀𝙎𝙋𝙀𝙍𝙀 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝘼 𝙌𝙐𝙀 𝙎𝙀 𝙀𝙉𝙑𝙄𝙀 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊\n\n➤ 𝙋𝙇𝙀𝘼𝙎𝙀 𝙒𝘼𝙄𝙏 𝙁𝙊𝙍 𝙏𝙃𝙀 𝙑𝙄𝘿𝙀𝙊 𝙏𝙊 𝘽𝙀 𝙎𝙀𝙉', m)
-    let xnxxLink = '';
-    if (args[0].includes('xnxx')) {
-      xnxxLink = args[0];
-    } else {
-      const index = parseInt(args[0]) - 1;
-      if (index >= 0) {
-        if (Array.isArray(global.videoListXXX) && global.videoListXXX.length > 0) {
-          const matchingItem = global.videoListXXX.find((item) => item.from === m.sender);
-          if (matchingItem) {
-            if (index < matchingItem.urls.length) {
-              xnxxLink = matchingItem.urls[index];
-            } else {
-              m.reply(`${lenguajeGB['smsAvisoFG']()}𝙉𝙊 𝙁𝙐𝙉𝘾𝙄𝙊𝙉𝙊, 𝙐𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙓𝙉𝙓𝙓, 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍\n\n𝘿𝙄𝘿𝙉𝙏 𝙒𝙊𝙍𝙆, 𝙐𝙎𝙀 𝘼𝙉 𝙓𝙉𝙓𝙓 𝙇𝙄𝙉𝙆, 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`)
-            }
-          } else {
-            m.reply(`${lenguajeGB['smsAvisoFG']()}𝙉𝙊 𝙁𝙐𝙉𝘾𝙄𝙊𝙉𝙊, 𝙐𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙓𝙉𝙓𝙓, 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍\n\n𝘿𝙄𝘿𝙉𝙏 𝙒𝙊𝙍𝙆, 𝙐𝙎𝙀 𝘼𝙉 𝙓𝙉𝙓𝙓 𝙇𝙄𝙉𝙆, 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`)
-          }
-        } else {
-          m.reply(`${lenguajeGB['smsAvisoFG']()}𝙉𝙊 𝙁𝙐𝙉𝘾𝙄𝙊𝙉𝙊, 𝙐𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙓𝙉𝙓𝙓, 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍\n\n𝘿𝙄𝘿𝙉𝙏 𝙒𝙊𝙍𝙆, 𝙐𝙎𝙀 𝘼𝙉 𝙓𝙉𝙓𝙓 𝙇𝙄𝙉𝙆, 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`)
-        }
-      }
-    }
-    const res = await xnxxdl(xnxxLink);
-    const json = await res.result.files;
-    conn.sendMessage(m.chat, {document: {url: json.high}, mimetype: 'video/mp4', fileName: res.result.title}, {quoted: m});
-  } catch {
-    m.reply(`${lenguajeGB['smsAvisoFG']()}𝙉𝙊 𝙁𝙐𝙉𝘾𝙄𝙊𝙉𝙊, 𝙐𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙓𝙉𝙓𝙓, 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍\n\n𝘿𝙄𝘿𝙉𝙏 𝙒𝙊𝙍𝙆, 𝙐𝙎𝙀 𝘼𝙉 𝙓𝙉𝙓𝙓 𝙇𝙄𝙉𝙆, 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`)
+    conn.reply(m.chat, `${tradutor.texto3}`, m);
+    const res = await xvideosdl(args[0]);
+    conn.sendMessage(m.chat, { document: { url: res.result.url }, mimetype: 'video/mp4', fileName: res.result.title }, { quoted: m });
+  } catch (e) {
+    throw `${tradutor.texto4}\n*◉ https://www.xvideos.com/video70389849/pequena_zorra_follada_duro*`;
   }
 };
-handler.command = /^(xnxxdl)$/i
-handler.level = 6
-handler.money = 1200
-handler.register = true
-export default handler
+handler.command = /^(xvideosdl)$/i;
+export default handler;
 
-async function xnxxdl(URL) {
+async function xvideosdl(url) {
   return new Promise((resolve, reject) => {
-    fetch(`${URL}`, {method: 'get'}).then((res) => res.text()).then((res) => {
-      const $ = cheerio.load(res, {xmlMode: false});
-      const title = $('meta[property="og:title"]').attr('content');
-      const duration = $('meta[property="og:duration"]').attr('content');
-      const image = $('meta[property="og:image"]').attr('content');
-      const videoType = $('meta[property="og:video:type"]').attr('content');
-      const videoWidth = $('meta[property="og:video:width"]').attr('content');
-      const videoHeight = $('meta[property="og:video:height"]').attr('content');
-      const info = $('span.metadata').text();
-      const videoScript = $('#video-player-bg > script:nth-child(6)').html();
-      const files = {
-        low: (videoScript.match('html5player.setVideoUrlLow\\(\'(.*?)\'\\);') || [])[1],
-        high: videoScript.match('html5player.setVideoUrlHigh\\(\'(.*?)\'\\);' || [])[1],
-        HLS: videoScript.match('html5player.setVideoHLS\\(\'(.*?)\'\\);' || [])[1],
-        thumb: videoScript.match('html5player.setThumbUrl\\(\'(.*?)\'\\);' || [])[1],
-        thumb69: videoScript.match('html5player.setThumbUrl169\\(\'(.*?)\'\\);' || [])[1],
-        thumbSlide: videoScript.match('html5player.setThumbSlide\\(\'(.*?)\'\\);' || [])[1],
-        thumbSlideBig: videoScript.match('html5player.setThumbSlideBig\\(\'(.*?)\'\\);' || [])[1]};
-      resolve({status: 200, result: {title, URL, duration, image, videoType, videoWidth, videoHeight, info, files}});
-    }).catch((err) => reject({code: 503, status: false, result: err}));
+    fetch(`${url}`, { method: 'get' })
+      .then(res => res.text())
+      .then(res => {
+        let $ = cheerio.load(res, { xmlMode: false });
+        const title = $("meta[property='og:title']").attr("content")
+        const keyword = $("meta[name='keywords']").attr("content")
+        const views = $("div#video-tabs > div > div > div > div > strong.mobile-hide").text() + " views"
+        const vote = $("div.rate-infos > span.rating-total-txt").text()
+        const likes = $("span.rating-good-nbr").text()
+        const deslikes = $("span.rating-bad-nbr").text()
+        const thumb = $("meta[property='og:image']").attr("content")
+        const url = $("#html5video > #html5video_base > div > a").attr("href")
+        resolve({ status: 200, result: { title, url, keyword, views, vote, likes, deslikes, thumb } })
+      })
+  })
+};
+
+async function xvideosSearch(url) {
+  return new Promise(async (resolve) => {
+    await axios.request(`https://www.xvideos.com/?k=${url}&p=${Math.floor(Math.random() * 9) + 1}`, { method: "get" }).then(async result => {
+      let $ = cheerio.load(result.data, { xmlMod3: false });
+      let title = [];
+      let duration = [];
+      let quality = [];
+      let url = [];
+      let thumb = [];
+      let hasil = [];
+
+      $("div.mozaique > div > div.thumb-under > p.title").each(function (a, b) {
+        title.push($(this).find("a").attr("title"));
+        duration.push($(this).find("span.duration").text());
+        url.push("https://www.xvideos.com" + $(this).find("a").attr("href"));
+      });
+      $("div.mozaique > div > div.thumb-under").each(function (a, b) {
+        quality.push($(this).find("span.video-hd-mark").text());
+      });
+      $("div.mozaique > div > div > div.thumb > a").each(function (a, b) {
+        thumb.push($(this).find("img").attr("data-src"));
+      });
+      for (let i = 0; i < title.length; i++) {
+        hasil.push({
+          title: title[i],
+          duration: duration[i],
+          quality: quality[i],
+          thumb: thumb[i],
+          url: url[i]
+        });
+      }
+      resolve(hasil);
+    });
   });
-}
+};
