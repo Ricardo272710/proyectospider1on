1142a171ff4df137cc975f2728eabcdccd233cb5
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-08-03 16:46:21 -0500

    Update anthony.js

--- plugins/anthony.js
+++ plugins/anthony.js
@@ -1,16 +1,12 @@
 const handler = async (m, {conn}) => {
-  m.reply(global.anthony);
+  m.reply(global.hola);
 };
-handler.command = /^(antho|anthony)$/i;
+handler.command = /^(hola|hola)$/i;
 export default handler;
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
-global.anthony = ` 
-╭┄〔 𝗧𝗶𝗺𝗲 𝙎𝙋𝙄𝘿𝙀𝙍 𝗕𝗼𝘁 〕┄⊱
-┊
-┊ ¿Se te olvidó que tú y yo somos fueguito🔥y me lo chupaste en tu cuarto?
-┊
-╰━⊰ Anthony ⊱━დ
+global.Fem4 = ` 
+* holaaaaaaa
 `;
