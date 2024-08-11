41b6f8b17fcbd3080bd02b65a5409b59c5037e80
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2024-08-10 22:40:48 -0600

    Actualizar descargas-ringtone.js

--- plugins/descargas-ringtone.js
+++ plugins/descargas-ringtone.js
@@ -12,7 +12,6 @@ export default handler
 async function ringtone(title) {
 return new Promise((resolve, reject) => {
 axios.get('https://meloboom.com/es/search/'+title).then((get) => {
-let $ = cheerio.load(get.data)
 let hasil = []
 $('#__next > main > section > div.jsx-2244708474.container > div > div > div > div:nth-child(4) > div > div > div > ul > li').each(function (a, b) {
 hasil.push({ title: $(b).find('h4').text(), source: 'https://meloboom.com/'+$(b).find('a').attr('href'), audio: $(b).find('audio').attr('src') })
