494dc8f44c5fcd50967f9e2b0e4161b11f404639
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-10-11 15:07:20 -0500

    Update ytmp44.js

--- plugins/ytmp44.js
+++ plugins/ytmp44.js
@@ -1,7 +1,72 @@
-{
-  "creator": "diego-ofc",
-  "title": "Charraska 2022 - La Bandida Car Audio - Dj Pepe & Jose Gonzalez (RMS CAR AUDIO)",
-  "author": "Dj Jose Gonzalez",
-  "video_url": "https://rr2---sn-p5qs7nzk.googlevideo.com/videoplayback?expire=1722932882&ei=MoqxZtftCOCKkucPvrOkmA8&ip=3.85.134.103&id=o-AIePvXsPkFA5aQMdouahdYnGfLRf8yNX0D-F3xrlD5TZ&itag=18&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=O6&mm=31%2C29&mn=sn-p5qs7nzk%2Csn-p5qlsnd6&ms=au%2Crdu&mv=m&mvi=2&pl=17&initcwndbps=5038750&spc=NO7bARhsAb-4T6KAR2vYrzvswfARdQ7f3tPvWFPMVQ-OU02XlxbSNhnLzv56&vprv=1&svpuc=1&mime=video%2Fmp4&rqh=1&cnr=14&ratebypass=yes&dur=1190.069&lmt=1693091053745049&mt=1722910892&fvip=5&c=ANDROID&txp=5318224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AJfQdSswRQIgfjfV9ES4qpi2LIS07tglb70ahZQaCct8gASi7Be195gCIQClV6X9B307vYx2K9GQeVHuTN39ZUSbYCNEES2Z5WQERQ%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AGtxev0wRQIhAKl97FYCt6P4jb41ORm08Z3uU8Zb20iNrnSosGZBCV_zAiA55AgfqXba04He-YyQVDcDf0D4OxEFVq61VuEZ6zbTrw%3D%3D",
-  "description": "LINK DE DESCARGA MP3\r\n\r\nhttps://www.mediafire.com/file/3z65s3896zl0rgd/Charraska_2022_-_La_Bandida_Car_Audio_-_Dj_Pepe_%2526_Jose_Gonzalez_%2528RMS_CAR_AUDIO%2529.mp3/file"
-}
+import fg from 'api-dylux'
+import yts from 'yt-search'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import fetch from 'node-fetch' 
+let limit = 100
+
+let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!args || !args[0]) return star.reply(m.chat, '💙 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
+if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✖️'))
+let q = args[1] || '360p'
+
+await m.react('🕓')
+try {
+let v = args[0]
+let yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
+let dl_url = await yt.video[q].download()
+let title = await yt.title
+let size = await yt.video[q].fileSizeH 
+let thumbnail = await yt.thumbnail
+
+let img = await (await fetch(`${thumbnail}`)).buffer()
+if (sizeMB.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
+	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
+       txt += `	💙   *Titulo* : ${title}\n`
+       txt += `	💙   *Calidad* : ${q}\n`
+       txt += `	💙   *Tamaño* : ${sizeMB}\n\n`
+       txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
+await m.react('✅')
+} catch {
+try {
+let yt = await fg.ytv(args[0], q)
+let { title, dl_url, size } = yt 
+let vid = (await yts(text)).all[0]
+let { thumbnail, url } = vid
+
+let img = await (await fetch(`${vid.thumbnail}`)).buffer()  
+if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
+	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
+       txt += `	💙   *Titulo* : ${title}\n`
+       txt += `	💙   *Calidad* : ${q}\n`
+       txt += `	💙   *Tamaño* : ${size}\n\n`
+       txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
+await m.react('✅')
+} catch {
+try {
+let yt = await fg.ytmp4(args[0], q)
+let { title, size, dl_url, thumb } = yt
+
+let img = await (await fetch(`${thumb}`)).buffer()
+if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
+	let txt = '`乂  Y O U T U B E  -  M P 4`\n\n'
+       txt += `	💙   *Titulo* : ${title}\n`
+       txt += `	💙   *Calidad* : ${q}\n`
+       txt += `	💙   *Tamaño* : ${size}\n\n`
+       txt += `> *- ↻ El video se esta enviando espera un momento, soy lenta. . .*`
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}}}
+handler.help = ['ytmp4 *<link yt>*']
+handler.tags = ['downloader']
+handler.command = ['ytmp4', 'ytv', 'yt']
+//handler.limit = 1
+handler.register = true 
+
+export default handler
