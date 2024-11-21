5dd35643140002a942fd7f56da62cc3082a33258
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-11-20 19:57:36 -0500

    Update bernita.js

--- plugins/bernita.js
+++ plugins/bernita.js
@@ -3,7 +3,7 @@ let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //8
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
 let img = 'https://qu.ax/uEFvz.jpeg' 
-let texto = `» *Quien quiere violar a este gay?* 🏳️‍🌈`
+let texto = `» *Quien quiere violar a este chota?* `
 
 const fkontak = {
 	"key": {
